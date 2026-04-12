01dfebdfefbb508e602e390318bf7ead0b68b9b4
Sebastian Resek <sebaresekk@gmail.com>
2026-04-12 15:54:50 -0300

    Hero

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import styles from './Hero.module.css';
 
 const Hero = () => {
+    const desktopBanner = "https://lh3.googleusercontent.com/u/0/d/1FlJXn4b-0otKs1uRxCRKpMTXvfBZOtFX";
+    const mobileBanner = "https://lh3.googleusercontent.com/u/0/d/1NNSMKYAcYUl-Ts1s1ZIufueM61X9M0vU";
+
     return (
-        /* Combinamos la clase de módulo con una global 'container' si la tienes */
         <section className={`${styles.heroSection} container`}>
-            
             <div className={`${styles.heroBannerContainer} animate-fade-in`}>
                 <div className={`${styles.mainBanner} glass`}>
-                    <img 
-                        src="https://www.enasport.com/cdn/shop/files/TransResveratrolDesk_1_962fb783-7cef-4395-8d1c-1a9d8ab73cfe.jpg?v=1775575457&width=1600" 
-                        alt="NutriPoint Banner" 
-                    />
+                    <picture>
+                        <source srcSet={mobileBanner} media="(max-width: 768px)" />
+                        <img 
+                            src={desktopBanner} 
+                            alt="NutriPoint Banner" 
+                            className={styles.heroImage}
+                        />
+                    </picture>
                     <div className={styles.bannerOverlay}></div>
                 </div>
             </div>
 
-            {/* Efecto decorativo */}
             <div className={styles.glowEffect}></div>
         </section>
     );
